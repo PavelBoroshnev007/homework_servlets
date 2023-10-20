15a3b0f56c1470655f65d69591048baa19cf0774
Pavel Boroshnev <pavelboroshnev9@gmail.com>
2023-10-20 23:15:17 +0300

    Добавил/отрефакторил код для внедрения зависимостей с помощью Spring

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,10 +1,27 @@
 package config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import controller.PostController;
 import org.springframework.context.annotation.Configuration;
+import repository.PostRepository;
+import repository.PostRepositoryImpl;
+import service.PostService;
 
 @Configuration
-@ComponentScan(basePackages = "controller")
 public class AppConfig {
-    // Нет необходимости создавать бины явно, Spring автоматически сканирует и создает компоненты.
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepositoryImpl();
+    }
+
+    @Bean
+    public PostService postService(PostRepository repository) {
+        return new PostService(repository);
+    }
+
+    @Bean
+    public PostController postController(PostService service) {
+        return new PostController(service);
+    }
 }
