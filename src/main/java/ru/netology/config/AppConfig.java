367c0a87003b52cfe387c21ca1e48e07fdfd3c3f
Pavel Boroshnev <pavelboroshnev9@gmail.com>
2023-10-22 10:55:34 +0300

    Доработал еще раз с учетом замечаний преподавателя

--- src/main/java/ru/netology/config/AppConfig.java
+++ src/main/java/ru/netology/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "ru/netology/controller")
+@ComponentScan(basePackages = "ru.netology.controller")
 public class AppConfig {
     // Нет необходимости создавать бины явно, Spring автоматически сканирует и создает компоненты.
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,37 +1,37 @@
 package ru.netology.servlet;
 
-import ru.netology.config.AppConfig;
 import ru.netology.controller.PostController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.AppConfig;
 @WebServlet(name = "MainServlet", urlPatterns = "/api/posts/*")
 public class MainServlet extends HttpServlet {
-    @Autowired
+    private static final String GET_METHOD = "GET";
+    private static final String POST_METHOD = "POST";
+    private static final String DELETE_METHOD = "DELETE";
+    private static final String API_PATH = "/api/posts";
+    private static final String ID_REGEX = "\\d+";
+
     private PostController controller;
 
-    public MainServlet(PostController controller) {
-        this.controller = controller;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        controller = context.getBean(PostController.class);
     }
 
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
-
-            final String GET_METHOD = "GET";
-            final String POST_METHOD = "POST";
-            final String DELETE_METHOD = "DELETE";
-
-            final String API_PATH = "/api/posts";
-            final String ID_REGEX = "\\d+";
 
             if (GET_METHOD.equals(method) && API_PATH.equals(path)) {
                 controller.all(resp);
